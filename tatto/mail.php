3eec4d1f43bece30c3616d615f155012da6f008b
vladislav0721 <3dspirit@mail.ru>
2017-09-06 23:37:51 +0600

    12

---
+++ tatto/mail.php
@@ -0,0 +1,43 @@
+<?php
+$mainform = $_POST['Заявка эскиз'];
+$name = $_POST['Имя'];
+$phone = $_POST['Телефон'];
+$message = $_POST['Сообщение'];
+$consultation = $_POST['Консультация'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'arman_kas96';                 // SMTP username
+$mail->Password = '12345a';                           // SMTP password
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+
+$mail->setFrom('arman_kas96@mail.ru');
+$mail->addAddress('3dspirit@mail.ru');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с сайта';
+$mail->Body    = 'Имя пользователя:';
+$mail->AltBody = 'Это сообщение в формате plain text';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+
+?>
